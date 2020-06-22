ba010c82e44a3e1446d4de0852936e16c606f419
AnyDev <794763733@qq.com>
2020-06-22 23:25:29 +0800

    repair page problem

--- src/router/index.js
+++ src/router/index.js
@@ -4,47 +4,42 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [{
+	path: "/login",
+	name: "Login",
+	component: () => import("../views/Login.vue")
+}, {
 	path: "/",
-	name: "index",
-	redirect: "management",
+	name: "Management",
+	component: () => import("../views/Management.vue"),
 	children: [{
-		path: "login",
-		name: "Login",
-		component: () => import("../views/Login.vue")
-	}, {
-		path: "management",
-		name: "Management",
-		component: () => import("../views/Management.vue"),
-		children: [{
-				path: "dashboard",
-				name: "Dashboard",
-				component: () => import("../views/Dashboard.vue")
-			},
-			{
-				path: "map",
-				name: "Map",
-				// route level code-splitting
-				// this generates a separate chunk (about.[hash].js) for this route
-				// which is lazy-loaded when the route is visited.
-				component: () =>
-					import( /* webpackChunkName: "about" */ "../views/Map.vue")
-			},
-			{
-				path: "table",
-				name: "Table",
-				component: () => import("../views/Table.vue")
-			},
-			{
-				path: "editor",
-				name: "Editor",
-				component: () => import("../views/Editor.vue")
-			}, {
-				path: "setup",
-				name: "Setup",
-				component: () => import("../views/Setup.vue")
-			},
-		]
-	}]
+			path: "dashboard",
+			name: "Dashboard",
+			component: () => import("../views/Dashboard.vue")
+		},
+		{
+			path: "map",
+			name: "Map",
+			// route level code-splitting
+			// this generates a separate chunk (about.[hash].js) for this route
+			// which is lazy-loaded when the route is visited.
+			component: () =>
+				import( /* webpackChunkName: "about" */ "../views/Map.vue")
+		},
+		{
+			path: "table",
+			name: "Table",
+			component: () => import("../views/Table.vue")
+		},
+		{
+			path: "editor",
+			name: "Editor",
+			component: () => import("../views/Editor.vue")
+		}, {
+			path: "setup",
+			name: "Setup",
+			component: () => import("../views/Setup.vue")
+		},
+	]
 }];
 
 const router = new VueRouter({
